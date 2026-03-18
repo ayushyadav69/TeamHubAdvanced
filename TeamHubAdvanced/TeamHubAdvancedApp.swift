56ffbf1b30faf3c25002dee9ca4ee79243f0a3a6
Ayush Yadav <ayush.dms.1415@gmail.com>
2026-03-18 12:16:34 +0530

    Initial Commit

---
+++ TeamHubAdvanced/TeamHubAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamHubAdvancedApp.swift
+//  TeamHubAdvanced
+//
+//  Created by Ayush yadav on 18/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TeamHubAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
